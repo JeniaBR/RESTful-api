722d05c71334fc86d011c105458f82d146650869
Jenia Brook <jenia@headstart.co.il>
2018-09-11 21:09:30 +0300

    parsing headers and payload via stream

--- index.js
+++ index.js
@@ -4,8 +4,9 @@
  */
 
 // Dependencies
-const http = require('http');
-const url = require('url');
+const http = require("http");
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 // The server should respond to all requests with the a string
 
@@ -15,7 +16,7 @@ const server = http.createServer((req, res) => {
 
   // Get the path
   const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g,'');
+  const trimmedPath = path.replace(/^\/+|\/+$/g, "");
 
   // Get the query string as an object
   const queryStringObject = parsedUrl.query;
@@ -23,16 +24,31 @@ const server = http.createServer((req, res) => {
   // Get the HTTP method
   const method = req.method.toLocaleLowerCase();
 
-  // Send the response 
-  res.end('Hello World\n');
+  // Get the headers as an object
+  const headers = req.headers;
 
-  // Log the request path
-  console.log(`Request is received from this path: ${trimmedPath} with method: ${method} and with these query string parameters ${JSON.stringify(queryStringObject)}`);
-  
+  // Get the payload, if any
+  const decoder = new StringDecoder("utf-8");
+  let buffer = "";
+
+  req.on("data", data => {
+    buffer += decoder.write(data);
+  });
+  req.on("end", () => {
+    buffer += decoder.end();
+
+    // Send the response
+    res.end("Hello World\n");
+
+    // Log the request path
+    console.log(
+      `Request received with this payload: ${JSON.stringify(buffer)}`
+    );
+  });
 });
 
 // Start the server, and have it listen on port 3000
 
 server.listen(3000, () => {
-  console.log('The server is listening on port 3000');
+  console.log("The server is listening on port 3000");
 });
